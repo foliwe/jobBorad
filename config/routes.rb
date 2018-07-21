2e6c50afbe2d4e1a71797e8c661dfdd5f9e652d3
foliwe <foliwe@gmail.com>
2018-07-21 03:06:04 +0100

    show page created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
     root to: 'home#index'
 
     get 'home/search'
+    get 'home/result'
     get 'home/job_directory'
+    get 'home/show'
+
 end
