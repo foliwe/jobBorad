b94352488940d9d821f7324de7aba7bc58cf6bd5
foliwe <foliwe@gmail.com>
2018-07-23 00:06:16 +0100

    title

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Job.destroy_all
+
 job1= Job.create!(title:"New Dentis Wanted",job_reference:"J180-A-18-230159",employer:'Central Advertising',department:'Surgery',salary_from:32000,salary_to:50000,job_type:'fixed',pay_scheme:'Other',function:'Asthma',note:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',description:'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
 job2= Job.create!(title:"Hot nurse wanted",job_reference:"N422-A-05-297451",employer:'Advertising',department:'Dentis',salary_from:20000,salary_to:25000, job_type:'permanent',pay_scheme:'All Night',function:'Care',note:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',description:'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
 
