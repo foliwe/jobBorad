612b46081ffd288b05b77c6c92335c4a8e5a04c5
foliwe <foliwe@gmail.com>
2018-07-23 00:17:46 +0100

    title field added

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180721175444) do
+ActiveRecord::Schema.define(version: 20180722231533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "jobs", force: :cascade do |t|
-    t.string  "title"
     t.string "job_reference"
     t.string "employer"
     t.string "department"
     t.string "pay_scheme"
-    t.string "salary_from"
-    t.string "salary_to"
+    t.decimal "salary_from", precision: 8, scale: 2
+    t.decimal "salary_to", precision: 8, scale: 2
     t.string "email"
     t.string "telephone"
     t.string "contact_name"
     t.string "function"
-    t.string "job_type"
+    t.string "job_type", default: "Negotiable"
     t.text "description"
     t.text "note"
     t.datetime "closing_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
