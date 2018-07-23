b9e43e1124235885db9c0bf080f7be1e64d5702c
foliwe <foliwe@gmail.com>
2018-07-23 01:27:23 +0100

    sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
 ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain         => 'sassltdjobboard.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
   :authentication => :plain,
+  :enable_starttls_auto => true
 }
-ActionMailer::Base.delivery_method = :smtp
