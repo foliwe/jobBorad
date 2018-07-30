90ebdf65f0eedb38b40ae15fad5ad7dda570ba1e
foliwe <foliwe@gmail.com>
2018-07-30 11:37:20 +0100

    fixed limite user on job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,12 +1,6 @@
 class Job < ApplicationRecord
   has_many :job_applications,dependent: :destroy
-  validate :limit_user_to_one_job_application
 
-  private
 
-  def limit_user_to_one_job_application
-    if user.job_applicaitons.count > 1
-      self.errors.add(:base, "You can only have one job application")
-    end
-  end
+  
 end
